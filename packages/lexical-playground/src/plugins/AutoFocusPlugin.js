78459646bb66d2e1556424408ddd18516dd97e08
Gerard Rovira <gerardrovira@fb.com>
2022-04-04 13:12:51 +0100

    typed something

--- packages/lexical-playground/src/plugins/AutoFocusPlugin.js
+++ packages/lexical-playground/src/plugins/AutoFocusPlugin.js
@@ -10,11 +10,36 @@
 import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
 import {useEffect} from 'react';
 
+import {Command} from '../../../lexical/flow/Lexical';
+
+type Event2 = {
+  x: boolean,
+};
+const formatBold: Command<string> = new Command<string>();
+const doEvent: Command<Event2> = new Command<Event2>();
+
 export default function AutoFocusPlugin(): null {
   const [editor] = useLexicalComposerContext();
 
   useEffect(() => {
     editor.focus();
+  }, [editor]);
+
+  useEffect(() => {
+    editor.execCommand2(formatBold, 'bold');
+    return editor.registerCommand2(formatBold, (boldString) => {
+      // eslint-disable-next-line no-unused-vars
+      const _x: string = boldString;
+      return true;
+    });
+  }, [editor]);
+
+  useEffect(() => {
+    editor.execCommand2(doEvent, {x: true});
+    return editor.registerCommand2(doEvent, (x) => {
+      console.log(x.x, x.y);
+      return true;
+    });
   }, [editor]);
 
   return null;
